2d9f09c30cca5906e5bc53c37db66220dbbd2352
Thold <tholuuduc@gmail.com>
2016-08-25 01:11:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,48 @@
-![Pokemap](https://i.imgur.com/CqmAekP.jpg)
-I've received a notice to cease and desist from Niantic Labs, and I've decided to comply with their requests. It was my intention to augment and improve the game experience of Pokemon Go, and we achieved just that! Without their blessing, I don't see myself having the motivation to continue this project.
 
-It grew from a 2 hour weekend project to a robust 2-click server ready-for-deployment in a matter of days. The project has inspired \~5 million views (500k uniques), 44k clones, 9.6k stars, 160 contributors, translation into 10 languages, and a wonderful community all in the span of 2 weeks. It's also been featured on The Verge, Arstechnica, Lifehacker, Stern.de, Business Insider, and many others. What was most exciting was seeing the hundreds of online community-run maps in towns and cities across the world. Some coffee shops even hosted copies for their own customers!
+# PokemonGo Map ![Python 2.7](https://img.shields.io/badge/python-2.7-blue.svg) [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/PoGoMapDev) [![Build Status](https://travis-ci.org/PokemonGoMap/PokemonGo-Map.svg?branch=develop)](https://travis-ci.org/PokemonGoMap/PokemonGo-Map)
 
-It's been my pleasure to get to know each of you guys: the core development team, the contributors, and the thousands that have gotten in touch with me! Niantic, my offer still stands to help you build an official game map!
 
--Ahmed Almutawa
+#[Twitter] (https://twitter.com/PokemapDevRE), [Website] (https://pokemongomap.github.io/PoGoMapWebsite/)#
 
-#### Edit:
-Thank you all for the kind words and offers of help! I'd like to clarify that the main reason I chose to shut it down was because I’ve lost interest rather than legal concerns. Niantic’s actions towards 3rd party developers have been very off-putting and it has killed my personal motivation to work on this project. This was a fun weekend project for a game I enjoyed, and now I’ve lost interest in both. I won’t resume development, but there are [active forks of this project][1] you could use.
+For general support, join [our discord server](https://discord.gg/uAmEkcu).
 
-[1]:	https://github.com/PokemonGoMap/PokemonGo-Map
+For feature requests go to, [our feathub page](http://feathub.com/PokemonGoMap/PokemonGo-Map).
+
+Live visualization of all the pokemon (with option to show gyms and pokestops) in your area. This is a proof of concept that we can load all the pokemon visible nearby given a location. Currently runs on a Flask server displaying Google Maps with markers on it.
+
+Features: 
+
+* Shows Pokemon, Pokestops, and gyms with a clean GUI.
+* Notifications 
+* Lure information
+* Multithreaded mode
+* Filters
+* Independent worker threads (many can be used simulatenously to quickly generate a livemap of a huge geographical area)
+* Localization (en, fr, pt_br, de, ru, ja, zh_tw, zh_cn, zh_hk)
+* DB storage (sqlite or mysql) of all found pokemon
+* Incredibly fast, efficient searching algorithm (compared to everything else available)
+
+[![Deploy](https://raw.githubusercontent.com/sych74/PokemonGo-Map-in-Cloud/master/images/deploy-to-jelastic.png)](https://jelastic.com/install-application/?manifest=https://raw.githubusercontent.com/sych74/PokemonGo-Map-in-Cloud/master/manifest.jps) [![Deploy on Scalingo](https://cdn.scalingo.com/deploy/button.svg)](https://my.scalingo.com/deploy?source=https://github.com/PokemonGoMap/PokemonGo-Map#develop)
+
+
+![Map](https://camo.githubusercontent.com/61d585e7706d136694f50ed2a092661b203a0a5d/687474703a2f2f70676d2e72656164746865646f63732e696f2f656e2f6c61746573742f5f696d616765732f636f7665722e706e67)
+
+## How to setup
+
+For instructions on how to setup and run the tool, please refer to the project [wiki](https://github.com/PokemonGoMap/PokemonGo-Map/wiki), or the [video guide](https://www.youtube.com/watch?v=2ACJHCNZ3ow).
+
+
+## Android Version
+
+There is an [Android port](https://github.com/omkarmoghe/Pokemap) in the works. All Android related prs and issues please refer to this [repo](https://github.com/omkarmoghe/Pokemap).
+
+## iOS Version
+
+There is an [iOS port](https://github.com/istornz/iPokeGo) in the works. All iOS related prs and issues please refer to this [repo](https://github.com/istornz/iPokeGo).
+
+
+## Contributions
+
+Please submit all pull requests to [develop](https://github.com/PokemonGoMap/PokemonGo-Map/tree/develop) branch.
+
+Building off [tejado's python pgoapi](https://github.com/tejado/pgoapi), [Mila432](https://github.com/Mila432/Pokemon_Go_API)'s API, [leegao's additions](https://github.com/leegao/pokemongo-api-demo/tree/simulation) and [Flask-GoogleMaps](https://github.com/rochacbruno/Flask-GoogleMaps). Current version relies primarily on the pgoapi and Google Maps JS API.
---
+++ runserver.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+import logging
+import time
+import re
+import requests
+
+from distutils.version import StrictVersion
+
+from threading import Thread, Event
+from queue import Queue
+from flask_cors import CORS
+from flask_cache_bust import init_cache_busting
+
+from pogom import config
+from pogom.app import Pogom
+from pogom.utils import get_args, insert_mock_data, get_encryption_lib_path
+
+from pogom.search import search_overseer_thread, fake_search_loop
+from pogom.models import init_database, create_tables, drop_tables
+
+# Currently supported pgoapi
+pgoapi_version = "1.1.7"
+
+# Moved here so logger is configured at load time
+logging.basicConfig(format='%(asctime)s [%(threadName)16s][%(module)14s][%(levelname)8s] %(message)s')
+log = logging.getLogger()
+
+# Make sure pogom/pgoapi is actually removed if it is an empty directory
+# This is a leftover directory from the time pgoapi was embedded in PokemonGo-Map
+# The empty directory will cause problems with `import pgoapi` so it needs to go
+oldpgoapiPath = os.path.join(os.path.dirname(__file__), "pogom/pgoapi")
+if os.path.isdir(oldpgoapiPath):
+    log.info("I found %s, but its no longer used. Going to remove it...", oldpgoapiPath)
+    shutil.rmtree(oldpgoapiPath)
+    log.info("Done!")
+
+# Assert pgoapi is installed
+try:
+    import pgoapi
+    from pgoapi import utilities as util
+except ImportError:
+    log.critical("It seems `pgoapi` is not installed. You must run pip install -r requirements.txt again")
+    sys.exit(1)
+
+# Assert pgoapi >= pgoapi_version
+if not hasattr(pgoapi, "__version__") or StrictVersion(pgoapi.__version__) < StrictVersion(pgoapi_version):
+    log.critical("It seems `pgoapi` is not up-to-date. You must run pip install -r requirements.txt again")
+    sys.exit(1)
+
+if __name__ == '__main__':
+    # Check if we have the proper encryption library file and get its path
+    encryption_lib_path = get_encryption_lib_path()
+    if encryption_lib_path is "":
+        sys.exit(1)
+
+    args = get_args()
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+
+    # Let's not forget to run Grunt / Only needed when running with webserver
+    if not args.no_server:
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), 'static/dist')):
+            log.critical('Missing front-end assets (static/dist) -- please run "npm install && npm run build" before starting the server')
+            sys.exit()
+
+    # These are very noisey, let's shush them up a bit
+    logging.getLogger('peewee').setLevel(logging.INFO)
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('pgoapi.pgoapi').setLevel(logging.WARNING)
+    logging.getLogger('pgoapi.rpc_api').setLevel(logging.INFO)
+    logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+    config['parse_pokemon'] = not args.no_pokemon
+    config['parse_pokestops'] = not args.no_pokestops
+    config['parse_gyms'] = not args.no_gyms
+
+    # Turn these back up if debugging
+    if args.debug:
+        logging.getLogger('requests').setLevel(logging.DEBUG)
+        logging.getLogger('pgoapi').setLevel(logging.DEBUG)
+        logging.getLogger('rpc_api').setLevel(logging.DEBUG)
+
+    # use lat/lng directly if matches such a pattern
+    prog = re.compile("^(\-?\d+\.\d+),?\s?(\-?\d+\.\d+)$")
+    res = prog.match(args.location)
+    if res:
+        log.debug('Using coordinates from CLI directly')
+        position = (float(res.group(1)), float(res.group(2)), 0)
+    else:
+        log.debug('Looking up coordinates in API')
+        position = util.get_pos_by_name(args.location)
+
+    # Use the latitude and longitude to get the local altitude from Google
+    try:
+        url = 'https://maps.googleapis.com/maps/api/elevation/json?locations={},{}'.format(
+            str(position[0]), str(position[1]))
+        altitude = requests.get(url).json()[u'results'][0][u'elevation']
+        log.debug('Local altitude is: %sm', altitude)
+        position = (position[0], position[1], altitude)
+    except (requests.exceptions.RequestException, IndexError, KeyError):
+        log.error('Unable to retrieve altitude from Google APIs; setting to 0')
+
+    if not any(position):
+        log.error('Could not get a position by name, aborting')
+        sys.exit()
+
+    log.info('Parsed location is: %.4f/%.4f/%.4f (lat/lng/alt)',
+             position[0], position[1], position[2])
+
+    if args.no_pokemon:
+        log.info('Parsing of Pokemon disabled')
+    if args.no_pokestops:
+        log.info('Parsing of Pokestops disabled')
+    if args.no_gyms:
+        log.info('Parsing of Gyms disabled')
+
+    config['LOCALE'] = args.locale
+    config['CHINA'] = args.china
+
+    app = Pogom(__name__)
+    db = init_database(app)
+    if args.clear_db:
+        log.info('Clearing database')
+        if args.db_type == 'mysql':
+            drop_tables(db)
+        elif os.path.isfile(args.db):
+            os.remove(args.db)
+    create_tables(db)
+
+    app.set_current_location(position)
+
+    # Control the search status (running or not) across threads
+    pause_bit = Event()
+    pause_bit.clear()
+
+    # Setup the location tracking queue and push the first location on
+    new_location_queue = Queue()
+    new_location_queue.put(position)
+
+    if not args.only_server:
+        # Gather the pokemons!
+        if not args.mock:
+            log.debug('Starting a real search thread')
+            search_thread = Thread(target=search_overseer_thread, args=(args, new_location_queue, pause_bit, encryption_lib_path))
+        else:
+            log.debug('Starting a fake search thread')
+            insert_mock_data(position)
+            search_thread = Thread(target=fake_search_loop)
+
+        search_thread.daemon = True
+        search_thread.name = 'search_thread'
+        search_thread.start()
+
+    if args.cors:
+        CORS(app)
+
+    # No more stale JS
+    init_cache_busting(app)
+
+    app.set_search_control(pause_bit)
+    app.set_location_queue(new_location_queue)
+
+    config['ROOT_PATH'] = app.root_path
+    config['GMAPS_KEY'] = args.gmaps_key
+
+    if args.no_server:
+        # This loop allows for ctrl-c interupts to work since flask won't be holding the program open
+        while search_thread.is_alive():
+            time.sleep(60)
+    else:
+        app.run(threaded=True, use_reloader=False, debug=args.debug, host=args.host, port=args.port)
